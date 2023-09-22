b84c50239596da4dc195a4ace03f98cc39255d1a
Thomas Stock <thomasstock1985@gmail.com>
2023-09-22 20:55:37 +0200

    vercel

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   ],
 
   build: {
+    emptyOutDir: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/main.ts"),
